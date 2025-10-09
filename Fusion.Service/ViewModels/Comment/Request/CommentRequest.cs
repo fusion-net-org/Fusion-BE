985b71adf98c4efc0b9b55291576abd47c876f85
BabyGau9 <caodung832003@gmail.com>
2025-10-09 22:38:21 +0700

    #71 fix

--- Fusion.Service/ViewModels/Comment/Request/CommentRequest.cs
+++ Fusion.Service/ViewModels/Comment/Request/CommentRequest.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 
 namespace Fusion.Service.ViewModels.Comment.Request
 {
     public class CommentRequest
     {
-        public long Id { get; ; }
+        public long Id { get; }
 
         public string? Body { get; set; }
 
