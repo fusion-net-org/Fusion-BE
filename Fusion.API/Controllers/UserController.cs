d2d4e793f16409bad28ff3c13bbae0eebd9dfd90
BabyGau9 <caodung832003@gmail.com>
2025-12-01 11:14:08 +0700

    oke

--- Fusion.API/Controllers/UserController.cs
+++ Fusion.API/Controllers/UserController.cs
@@ -12,7 +12,6 @@ using Fusion.Service.ViewModels.Users.Responses;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Claims;
 using Fusion.Service.ViewModels.UserLog.Responses;
 
 namespace Fusion.API.Controllers
