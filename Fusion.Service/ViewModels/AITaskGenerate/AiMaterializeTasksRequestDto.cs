c71db546f38815e18db09d2fa015a4cb13a09e53
unknown <duyng21303@gmail.com>
2025-12-04 20:51:06 +0700

    #299 handle fillter

---
+++ Fusion.Service/ViewModels/AITaskGenerate/AiMaterializeTasksRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fusion.Service.ViewModels.AITaskGenerate
+{
+    public sealed class AiMaterializeTasksRequestDto
+    {
+        public Guid ProjectId { get; set; }
+
+        public Guid? DefaultSprintId { get; set; }
+
+    
+        public List<AiGeneratedTaskDraftDto> Tasks { get; set; } = new();
+    }
+}
