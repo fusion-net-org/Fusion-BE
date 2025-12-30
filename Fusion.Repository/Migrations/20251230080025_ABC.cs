e45109456aea60560b1713c5f969fe8646af7ab4
nguyenkienminh <kienminh04122003@gmail.com>
2025-12-30 15:07:46 +0700

    #351 fix: DB

---
+++ Fusion.Repository/Migrations/20251230080025_ABC.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fusion.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class ABC : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "update_at",
+                table: "ProjectTasks",
+                type: "datetime2(3)",
+                precision: 3,
+                nullable: true,
+                defaultValueSql: "(sysutcdatetime())",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2(3)",
+                oldPrecision: 3,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "update_at",
+                table: "ProjectTasks",
+                type: "datetime2(3)",
+                precision: 3,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2(3)",
+                oldPrecision: 3,
+                oldNullable: true,
+                oldDefaultValueSql: "(sysutcdatetime())");
+        }
+    }
+}
